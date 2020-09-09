f02dc3f22a000c6343f2336b51d90f9489038341
Gregg Pollack <greggpollack+github@gmail.com>
2020-09-09 16:08:48 -0400

    Add new endpoint & update title

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Vue_Router',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
