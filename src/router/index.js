6ad3846565b91e6e6240a1f6293bf36794412638
Gregg Pollack <greggpollack+github@gmail.com>
2020-09-14 17:30:20 -0400

    Added new event routes with navigation duplicated

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import About from '@/views/About.vue'
 import EventList from '@/views/EventList.vue'
-import EventDetails from '@/views/EventDetails.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
 
 const routes = [
   {
@@ -14,6 +16,18 @@ const routes = [
     name: 'EventDetails',
     props: true,
     component: EventDetails
+  },
+  {
+    path: '/event/:id/register',
+    name: 'EventRegister',
+    props: true,
+    component: EventRegister
+  },
+  {
+    path: '/event/:id/edit',
+    name: 'EventEdit',
+    props: true,
+    component: EventEdit
   },
   {
     path: '/about',
