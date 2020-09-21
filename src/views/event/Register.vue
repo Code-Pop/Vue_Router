fa3fd407f29e76ef8df5d9e50510bc1fcb8c7ee6
Gregg Pollack <greggpollack+github@gmail.com>
2020-09-21 16:36:21 -0400

    Added ability for flash message, with global store

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,24 @@
 <template>
   <p>Regstration form here</p>
+  <button @click="register">Register Me!</button>
 </template>
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 4000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
